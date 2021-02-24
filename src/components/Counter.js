236627e0f971ec1e8f3680912aae07a7b9c019eb
Marina Monsivais <minamonsi@me.com>
2021-02-24 11:58:50 -0600

    Completed steps 1, 2, & 3

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,10 +46,11 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React, {useState} from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 - Imported state hook */
 
 export default function Counter() {
-  /* STEP 1 */
+  /* STEP 1 - Created count state */
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     /* STEP 4 */
@@ -64,14 +65,14 @@ export default function Counter() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: count % 2===0 ? 'royalblue' : 'crimson', /* STEP 2 - Ternary expression written */
   };
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is {count % 2===0 ? `even` : `odd`/* STEP 3-Changed 0 to count & added ternary expression*/}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
